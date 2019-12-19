7a656f0eeff2dcf11a25c1aa9d16874e5504552f
Yuewen.Huang <Yuewen.Huang@geely.com>
2019-12-19 19:12:09 +0800

    repo coding

--- repo/src/dag.go
+++ repo/src/dag.go
@@ -1,7 +1,7 @@
 package repo
 
 type dag struct {
-
+	
 }
 
 
--- repo/src/repo.go
+++ repo/src/repo.go
@@ -3,62 +3,80 @@
  *
  *
  */
-
 package core
 
 import (
 	"os"
 	"io/ioutil"
+	"log"
+	"gopkg.in/src-d/go-git.v4"
 )
 
-<<<<<<< HEAD
-const(
-	ROOT_PATH = ""
-=======
 const (
-	DEPENDECY = 1 
+	REPO_ROOT_DIR = ""
 )
 
-struct Repo interface {
->>>>>>> e5908ba09c2523fe91561d83c1702fa236bcda55
+type Repo struct {
 
-	repoDict = {}
-)
+	// unique
+	url string
 
+	projectName string
 
-type Command interface {
-
-	func Execute(args ... string)
 }
 
 
-// 同步
-// return path
-func (t *Command) Sync(repoCode string) string{
-	// repoCode --> {repoUrl,repoPath,repoDir}
-	repodir = repoUrl()
+func NewRepo(url string,srcCommit string,targetCommit string) Repo{
+	var repo = Repo{url,"hello"}
+	return repo
+}
 
+// return path
+// repoCode 由group + service组成
+func (r Repo) Sync() bool{
+	path = ROOT + os.PathSeparator +r.projectName
+	log.println
+	exists,err := PathExists(path) 
+	if !exists {
+		os.Mkdir(path)
+		exec.Comman("git clone ",path)
+		exec.Comman()
+	}
 	//exec sych.sh
 	var cmd = exec.Command(repoUrl,)
 	cmd.Run()
 	cmd.stdout
 }
 
+
+func PathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
+
 // output string
-func (t *Command) Diff(srcCommit string,targetCommit string) string{
+func (r Repo) Diff(srcCommit string,targetCommit string) []string{
 
 }
 
 
-func ReadFile(fileId string) string {
+// 
+func (r Repo) ReadFile(repoCode string,fileId string) string {
 	// get filepath by fileId
 
 	var content 
 
-
-
 }
 
+
+// 缓存文件id与文件路径的关系
 
 
 
