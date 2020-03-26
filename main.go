46c84e336a0df23f43cc16e45215a09941fde26d
huangyuewen <vhypnus@gmail.com>
2020-03-26 16:29:52 +0800

    update

--- main.go
+++ main.go
@@ -4,26 +4,22 @@ import (
 	"log"
 	"fmt"
 	"strings"
+    "ohain/util"
 )
 
 func main(){
-	s := "abc你好"
-    r := "123你好"
-    fmt.Println("len(s)=", len([]byte(s)), "len(r)=", len([]rune(r))) //len(s)= 9 len(r)= 5
+	
  
-    // for k, v := range r {
-    //     fmt.Println("k=", k, "v=", v)
-    // }
- 
-    // for k, v := range []rune(r) {
-    //     fmt.Println("k2=", k, "v2=", v)
-    // }
+    util.Assert(1,1)
 }
 
 func trimspace(){
 	var s = "  hello world   "
 	fmt.Println(s)
 	fmt.Println(strings.TrimSpace(s))
+    s := "abc你好"
+    r := "123你好"
+    fmt.Println("len(s)=", len([]byte(s)), "len(r)=", len([]rune(r))) //len(s)= 9 len(r)= 5
 }
 
 
--- project/go.mod
+++
@@ -1,5 +0,0 @@
-module ohain/project
-
-go 1.14
-
-require github.com/vhypnus/ohain/assert v0.0.0-20200321093600-a55b1e96ad8e
